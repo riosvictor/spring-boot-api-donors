9d0d594ce76fc117e78e4bedbbb96b0562f6a5dd
Paulo Victor Rios <victorios.ba@gmail.com>
2020-02-22 08:50:54 -0300

    Show Id in JSON

--- src/main/java/com/api/donors/repository/DonorRepository.java
+++ src/main/java/com/api/donors/repository/DonorRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "donors", path = "donors")
 public interface DonorRepository extends MongoRepository<Donor, String> {
