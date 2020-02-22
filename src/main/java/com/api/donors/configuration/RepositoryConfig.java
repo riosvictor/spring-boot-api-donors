5d1cf69652c1458714d370d743ceb616278f5963
Paulo Victor Rios <victorios.ba@gmail.com>
2020-02-22 17:09:52 -0300

    Tratamento CORS + remoção warnings

--- src/main/java/com/api/donors/configuration/RepositoryConfig.java
+++ src/main/java/com/api/donors/configuration/RepositoryConfig.java
@@ -3,11 +3,10 @@ package com.api.donors.configuration;
 import com.api.donors.model.Donor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
-import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;d
 
 @Configuration
-public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
-    @Override
+public class RepositoryConfig implements RepositoryRestConfigurer {
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         config.exposeIdsFor(Donor.class);
     }
